6592dd87e645270d990db290c8a2e13a42fcd6ff
Shivang Soni <shivangsoni1998@gmail.com>
2025-11-03 11:54:48 -0500

    feat: add eslint plugin for tailwindcss to enforce tailwind css related rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = {
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier', 'plugin:tailwindcss/recommended'],
     plugins: ['prettier'],
     rules: {
       'prettier/prettier': 'error',
