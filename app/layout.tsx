d7d75326404c4184427114cb66d693dff9dfca72
Shivang Soni <shivangsoni1998@gmail.com>
2025-11-05 10:34:31 -0500

    feat: add nav links and stylings in the header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
-import type { Metadata } from "next";
-import { Source_Sans_3 as FontSans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Source_Sans_3 as FontSans } from 'next/font/google';
+
+import Header from '@/components/common/header';
+import Footer from '@/components/common/footer';
+import './globals.css';
 
 const fontSans = FontSans({
-  variable: "--font-sans",
-  subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "InsightPDF – AI-Powered PDF Summarizer & Analyzer",
-  description: "Summarize, analyze, and extract insights from PDFs instantly using AI",
+  title: 'InsightPDF – AI-Powered PDF Summarizer & Analyzer',
+  description:
+    'Summarize, analyze, and extract insights from PDFs instantly using AI',
 };
 
 export default function RootLayout({
@@ -19,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`font-sans ${fontSans.variable} antialiased`}
-      >
-        {children}
+    <html lang='en'>
+      <body className={`font-sans ${fontSans.variable} antialiased`}>
+        <div className='relative flex min-h-screen flex-col'>
+          <Header />
+          <main className='flex-1'>{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans'>
-      <Button variant="outline">Shadcn Button</Button>
+    <div className=''>
+      <Button variant='outline'>Shadcn Button</Button>
     </div>
   );
 }
