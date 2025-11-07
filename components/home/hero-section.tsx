d4c667b71464a17cfcf81839dc635205f9be22f2
Shivang Soni <shivangsoni1998@gmail.com>
2025-11-07 10:20:43 -0500

    feat: add description and try insight-pdf  button and stylings in hero section component

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -18,13 +18,24 @@ const HeroSection = () => {
         </div>
       </div>
       <h1 className='font-bold py-6 text-center'>
-        Turn PDFs into sharp, easy-to-digest summaries
+        Turn PDFs into sharp,{' '}
+        <span className='relative inline-block'>
+          <span className='relative z-10 px-2'>easy-to-digest</span>
+          <span
+            className='absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1'
+            area-hidden='true'
+          ></span>
+        </span>
+        summaries
       </h1>
       <h2 className='text-lg sm:text-xl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600'>
         Watch your document come alive with a beautiful summary reel in seconds
       </h2>
       <div>
-        <Button variant='link' className='text-white'>
+        <Button
+          variant='link'
+          className='text-white mt-6 text-base sm:text-lg lg:text-xl px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 rounded-full lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg transition-all duration-300'
+        >
           <Link href='/#pricing' className='flex gap-2 items-center'>
             <span>Try InsightPDF</span>
             <ArrowRight className='animate-pulse' />
